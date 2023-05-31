58ca0edf1821ef188df81d1321583e0dcc6f8431
mcanabrava <mcanabrava@github.com>
2023-05-30 23:48:41 -0300

    finish renaming containers

--- Evaluate-Human-Balance-with-Spark-Streaming/project/starter/submit-event-kafkajoin.sh
+++ Evaluate-Human-Balance-with-Spark-Streaming/project/starter/submit-event-kafkajoin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it 2evaluatehumanbalancewithsparkstreaming-spark-1 /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 /home/workspace/project/starter/sparkpykafkajoin.py | tee ../../spark/logs/kafkajoin.log 
+docker exec -it evaluate-human-balance-with-spark-streaming-spark-1 /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 /home/workspace/project/starter/sparkpykafkajoin.py | tee ../../spark/logs/kafkajoin.log 
